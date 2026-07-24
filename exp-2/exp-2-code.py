7ff679f53bb0beca9e73028edd8e32ae82edaf21
ButraMahesh <192425338.simats@saveetha.com>
2026-07-24 09:41:05 +0530

    Add files via upload

---
+++ exp-2/exp-2-code.py
@@ -0,0 +1,10 @@
+import cv2
+
+image = cv2.imread("image.jpg")
+blur = cv2.GaussianBlur(image, (15, 15), 0)
+
+cv2.imshow("Original Image", image)
+cv2.imshow("Blurred Image", blur)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
